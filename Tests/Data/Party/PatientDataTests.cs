4f0696315cc3540caed24ccbe5a4e0004e4d7f03
unknown <eleinp@ttu.ee>
2022-02-17 14:01:47 +0200

    update

---
+++ Tests/Data/Party/PatientDataTests.cs
@@ -0,0 +1,17 @@
+﻿using EMEHospitalWebApp.Data.Party;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EMEHospitalWebApp.Tests.Data.Party
+{
+    [TestClass]
+    public class PatientDataTests : BaseTests<PatientData>
+    {
+        [TestMethod] public void IdTest() => isProperty<string>();
+        [TestMethod] public void FirstNameTest() => isProperty<string?>();
+        [TestMethod] public void LastNameTest() => isProperty<string?>();
+        [TestMethod] public void GenderTest() => isProperty<string?>();
+        [TestMethod] public void BirthDateTest() => isProperty<DateTime?>();
+        [TestMethod] public void IdCodeTest() => isProperty<string?>();
+    }
+}
---
+++ Tests/Facade/Party/PatientViewTests.cs
@@ -0,0 +1,19 @@
+﻿using EMEHospitalWebApp.Facade.Party;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EMEHospitalWebApp.Tests.Facade.Party
+{
+    [TestClass]
+    public class PatientViewTests : BaseTests<PatientView>
+    {
+        [TestMethod] public void IdTest() => isProperty<string>();
+        [TestMethod] public void FirstNameTest() => isProperty<string?>();
+        [TestMethod] public void LastNameTest() => isProperty<string?>();
+        [TestMethod] public void GenderTest() => isProperty<string?>();
+        [TestMethod] public void BirthDateTest() => isProperty<DateTime?>();
+        [TestMethod] public void IdCodeTest() => isProperty<string?>();
+        [TestMethod] public void FullNameTest() => isProperty<string>();
+        
+    }
+}
