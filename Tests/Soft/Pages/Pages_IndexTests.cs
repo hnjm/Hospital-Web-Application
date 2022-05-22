8957e8034065623082f7fd06b28e4cbbcff13f9d
honogu <holdernq@gmail.com>
2022-05-23 00:06:29 +0300

    update

--- Tests/Soft/Pages/Pages_IndexTests.cs
+++ Tests/Soft/Pages/Pages_IndexTests.cs
@@ -5,7 +5,7 @@ namespace EMEHospitalWebApp.Tests.Soft.Pages {
     [TestClass] public class Pages_IndexTests : IndexTests {
         [TestMethod] public async Task HomeIndexTest() {
             var html = await getHtmlPage("");
-            isTrue(html.Contains("Holgeri ning Eliise veebirakendus"));
+            isTrue(html.Contains("Sinu usaldusväärseim e-haigla"));
         }
     }
 }
